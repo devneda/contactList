3d47e0af0d2c5bc48dd4d6e3e24b045edbd2e55b
devneda <bprk9529@gmail.com>
2025-06-12 23:23:30 +0200

    Se añaden los primeros test unitarios y de integracion para Contacts, y primera estructura orientado a la factorización del backend

--- backend/src/test/integration/contacts.test.js
+++ backend/src/test/integration/contacts.test.js
@@ -23,7 +23,66 @@ describe('contacts' , () => {
                     
                     // Se espera que los primeros datos de la respuesta sean estos 
                     expect(response.body[0].name).to.equal('Carlos');
-                    expect(response.body[1].name).to.equal('Ainhoa');
+                    expect(response.body[1].name).to.equal('Ainho');
+                    done();
+                });
+        });
+    });
+
+    describe('POST /contacts', () => {
+        it('should register a new contact', (done) => {
+            chai.request(app)
+                .post('/contacts')
+                .send({
+                    name: 'contactName',
+                    lastname: 'lastname',
+                    phone: '1234567',
+                    email: 'example@example.dev',
+                    birthday: '1995-10-10'
+                })
+                .end((error, response) => {
+                    response.should.have.status(201);
+                    expect(response.body).to.have.property('id');
+                    expect(response.body).to.have.property('name');
+                    expect(response.body).to.have.property('lastname');
+                    expect(response.body).to.have.property('phone');
+                    expect(response.body).to.have.property('email');
+                    expect(response.body).to.have.property('birthday');
+                    done();
+                });
+        });
+
+        it('validation should fail because name is mandatory', (done) => {
+            chai.request(app)
+                .post('/contacts')
+                .send({
+                    lastname: 'lastname',
+                    phone: '1234567',
+                    email: 'example@example.dev',
+                    birthday: '1995-10-10'
+                })
+                .end((error, response) => {
+                    response.should.have.status(400);
+                    expect(response.body.status).to.have.equal('bad-request');
+                    expect(response.body.message).to.have.equal('Faltan campos obligatorios');                    
+                    done();
+                });
+        });
+
+        it('validation should fail because phone must be a number', (done) => {
+            chai.request(app)
+                .post('/contacts')
+                .send({
+                    name: 'contactName1',
+                    lastname: 'lastname',
+                    phone: '1234567L',
+                    email: 'example@example.dev',
+                    birthday: '1995-10-10'
+                })
+                .end((error, response) => {
+                    response.should.have.status(400);
+                    expect(response.body.status).to.have.equal('bad-request');
+                    expect(response.body.message).to.have.equal('El campo phone debe contener solo números');                    
                     done();
                 });
         });
